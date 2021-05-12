03bb2224d2a8538f9c5bed51a3107c27a9eee63c
AleexWalker <aluurs4039@ieselcaminas.org>
2021-05-12 19:08:22 +0200

    Ejercicio5Resuelto

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -13,11 +13,12 @@ public class Ejercicio5 {
             FileReader fichero = new FileReader("/home/administrador/Escritorio/Lector/" + entrada);
             BufferedReader lector = new BufferedReader(fichero);
 
-            String linea;
-            while ((linea = lector.readLine()) != null) {
-                lector.skip(2);
-                System.out.println(linea);
-            }
+            int caracter = 0;
+            do {
+                fichero.skip(2);
+                System.out.print((char) caracter);
+                caracter = fichero.read();
+            } while (caracter != -1);
         } catch (Exception e) {
             System.out.println("Fichero no leído");
         }
