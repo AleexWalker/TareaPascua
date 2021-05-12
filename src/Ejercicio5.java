865c495a26ad974e484160c24d11f0abb082f624
AleexWalker <aluurs4039@ieselcaminas.org>
2021-05-12 19:01:33 +0200

    Ejercicio5

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,2 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Ejercicio5 {
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("¿Cuál es el archivo? ");
+        String entrada = teclado.next();
+
+        try {
+            FileReader fichero = new FileReader("/home/administrador/Escritorio/Lector/" + entrada);
+            BufferedReader lector = new BufferedReader(fichero);
+
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                lector.skip(2);
+                System.out.println(linea);
+            }
+        } catch (Exception e) {
+            System.out.println("Fichero no leído");
+        }
+    }
 }
